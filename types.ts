9e388a4d158e7bd575659f9671c810f889f41181
Luciano Amâncio dos Santos <madruga665@hotmail.com>
2022-01-06 13:21:33 -0300

    aula 5 tipos

---
+++ types.ts
@@ -0,0 +1,51 @@
+// boolean (true / false)
+let isOpen: boolean;
+isOpen = true;
+
+// string ('xablau')
+let message: string;
+message = `xabalu ${isOpen}`;
+
+// number (int, float, hex, binary)
+let total: number;
+total = 0xff0;
+
+// array (type[])
+let items: string[];
+items = ["teste", "xablau"];
+
+let values: Array<number>;
+values = [1, 2, 3];
+
+// tuple
+let title: [number, string, boolean];
+title = [1, "Xablau", true];
+
+// enum
+enum Colors {
+  white = "#fff",
+  black = "#000",
+}
+
+// any (qualquer coisa) NÃO É LEGAL!
+let coisa: any;
+coisa = true;
+coisa = "xablau";
+coisa = 5;
+
+// void (vazio)
+function logger(): void {
+  console.log("xablau");
+}
+
+// null | undefined
+type Bla = string | undefined;
+
+// never
+function error(): never {
+  throw new Error("error");
+}
+
+// object
+let cart: object;
+cart = { chave: "valor" };
